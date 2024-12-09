2ab452dd3a0e2dc68e047a36e75257cada920af5
WhosWiFi <whoswifi@gmail.com>
2024-12-08 19:43:01 -0500

    progress

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ app.get('/', function (req, res) {
       });
 });
 
+app.get('/apps', function (req, res) {
+  fs.readFile('apps.html', function (err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+});
+
 app.listen(5123, function () {
   console.log('Example app listening on port 5123!');
 });
