900909f863b30f556cb01b6e939aa4943ed3b32c
WhosWiFi <whoswifi@gmail.com>
2024-12-10 17:45:07 -0500

    about and contact

--- app.js
+++ app.js
@@ -153,12 +153,13 @@ app.post('/create_post', function (req, res) {
 <body>
   <nav>
     <ul class="nav-links">
+      <li><a href="/">Home</a></li>
       <li><a href="/apps">Apps</a></li>
       <li><a href="/blog">Blog</a></li>
       <li><a href="/shop">Shop</a></li>
-      <li><a href="#about">About</a></li>
-      <li><a href="#contact">Contact</a></li>
-      <li><a href="#login">Login/Register</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/contact">Contact</a></li>
+      <li><a href="/login">Login/Register</a></li>
     </ul>
   </nav>
 
