a8dae95f8ca395f42fa9d1edacf0bc6b51dcdb85
WhosWiFi <whoswifi@gmail.com>
2024-12-11 20:13:09 -0500

    fixed home button on edit blog and maybe fixed autofill on terminal search

--- app.js
+++ app.js
@@ -335,12 +335,13 @@ app.get('/blogs/:id/edit', function (req, res) {
       <body>
         <nav>
           <ul class="nav-links">
+            <li><a href="/">Home</a></li>
             <li><a href="/apps">Apps</a></li>
             <li><a href="/blog">Blog</a></li>
-            <li><a href="#shop">Shop</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#login">Login/Register</a></li>
+            <li><a href="/shop">Shop</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/login">Login/Register</a></li>
           </ul>
         </nav>
         <div class="edit-form">
