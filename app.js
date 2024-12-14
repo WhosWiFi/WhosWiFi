a4dea378b95e5470cd53f930f7035afd3ee15951
WhosWiFi <WhosWiFi@gmail.com>
2024-12-13 22:50:03 -0500

    start of tetris game

--- app.js
+++ app.js
@@ -449,6 +449,18 @@ app.get('/api/ip', function (req, res) {
   });
 });
 
+app.get('/tetris', function (req, res) {
+  fs.readFile('tetris.html', function (err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end('Tetris not found');
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+});
+
 app.listen(5123, function () {
   console.log('Example app listening on port 5123!');
 });
