c5ce1010e2b781ca415f81363e9fb49117c4281c
WhosWiFi <WhosWiFi@gmail.com>
2024-12-23 17:20:32 -0500

    sdf

--- app.js
+++ app.js
@@ -38,6 +38,14 @@ console.log('Database Config:', {
   // Don't log the password for security
 });
 
+app.get('/registerPage', function (req, res) {
+  fs.readFile('registration.html', function (err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+});
+
 app.post('/register', (req, res) => {
   const { username, password, color = 'white' } = req.body; // Default color if not provided
 
